edd2812fc10605d03b6bf30fdfa9b4e73cd3eff8
Zuzana Dostalova <dostalovaz@gmail.com>
2017-05-10 00:03:58 +0300

    Initial commit - moving 3 hafici

---
+++ Assets/CharacterSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSwitcher : MonoBehaviour {
+
+	public GameObject Olaf;
+	public GameObject Haley;
+	public GameObject Lulu;
+
+	private GameObject _currentHafik;
+
+	// Use this for initialization
+	void Start() {
+		_currentHafik = Olaf;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		var movement = Input.GetAxisRaw("Horizontal");
+
+		if (Input.GetKey(KeyCode.O)) {
+			_currentHafik = Olaf;
+		} else if (Input.GetKey(KeyCode.L)) {
+			_currentHafik = Lulu;
+		} else if (Input.GetKey(KeyCode.H)) {
+			_currentHafik = Haley;
+		}
+
+		_currentHafik.GetComponent<HafikController>().Move(movement);
+		_currentHafik.GetComponent<HafikController>().FollowCamera();
+	}
+}
---
+++ Assets/HafikController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HafikController : MonoBehaviour {
+	public float walkSpeed = 3;
+	public Transform cameraTransform;
+	public float jumpForce = 4;
+
+	private Rigidbody2D _rigidbody2D;
+	private Transform _leftRaySource;
+	private Transform _rightRaySource;
+
+	private Animator _anim;
+	private SpriteRenderer _renderer;
+
+	private bool _grounded = false;
+	private bool _doubleJumpAvailable = false;
+	private bool _followCamera = false;
+
+	// Use this for initialization
+	void Start () {
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+		_leftRaySource = transform.FindChild("Feet Raycast Left");
+		_rightRaySource = transform.FindChild("Feet Raycast Right");
+		_anim = GetComponent<Animator>();
+		_renderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void FollowCamera() {
+		_followCamera = true;
+	}
+
+	public void Move(float movement) {
+		if (movement == 1) {
+			_renderer.flipX = false;
+			_anim.SetBool("isMoving", true);
+		} else if (movement == -1) {
+			_renderer.flipX = true;
+			_anim.SetBool("isMoving", true);
+		} else {
+			_anim.SetBool("isMoving", false);
+		}
+
+		_rigidbody2D.velocity = new Vector2(movement*walkSpeed, _rigidbody2D.velocity.y);
+
+		HandleJump();
+
+		GroundCheckRay(_leftRaySource);
+		GroundCheckRay(_rightRaySource);
+	}
+
+	private void GroundCheckRay(Transform raySource) {
+		var from = raySource.transform.position;
+		float feetRayLength = 0.03f;
+
+		Debug.DrawLine(from, from + Vector3.down*feetRayLength);
+		var hit = Physics2D.Raycast(from, Vector2.down, feetRayLength);
+		if (hit) {
+			_grounded = true;
+		} else {
+			_grounded = false;
+		}
+	}
+
+	private IEnumerator EnableDoubleJump() {
+		yield return new WaitForSeconds(0.05f);
+		_doubleJumpAvailable = true;
+	}
+
+	private void HandleJump() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (_grounded) {
+				_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+				_rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+				StartCoroutine(EnableDoubleJump());
+			} else if (_doubleJumpAvailable) {
+				_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+				_rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+				_doubleJumpAvailable = false;
+			}
+		}
+	}
+
+	void LateUpdate() {
+		if (_followCamera) {
+			cameraTransform.position = new Vector3(transform.position.x + 3, transform.position.y,
+				cameraTransform.position.z);
+			_followCamera = false;
+		}
+	}
+}
